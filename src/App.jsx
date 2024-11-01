468808b4529349393387ce90fc2c58702c567518
Tetiana Klitna <klitna.tetiana@gmail.com>
2024-11-01 11:39:50 -0700

    id: unique identifier (ex. 1, 2, 3)

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import './App.css'
 
 const todoList = [
   {
-    objectId: '1',
+    id: 1,
     title: "Start assignment"
   }, 
   {
-    objectId: '2',
+    id: 2,
     title: "Run assignment"
   }, 
   {
-    objectId: '3',
+    id: 3,
     title: "Complete assignment"    
   }
 ];
