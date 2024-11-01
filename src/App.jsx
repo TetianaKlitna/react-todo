3408abb2f03d4552f3df5db53319f49b580c7a1c
Tetiana Klitna <klitna.tetiana@gmail.com>
2024-11-01 12:08:07 -0700

    moved const list to separate file

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,12 @@
 import './App.css'
-
-const todoList = [
-  {
-    id: 1,
-    title: "Start assignment"
-  }, 
-  {
-    id: 2,
-    title: "Run assignment"
-  }, 
-  {
-    id: 3,
-    title: "Complete assignment"    
-  }
-];
+import { todoList } from './constants/todoList';
 
 function App() {
   return (
       <div>
         <h1>Todo List</h1>
         <ul>
-           {todoList.map(item => <li key = {item.objectId}>{item.title}</li>)}
+           {todoList.map(item => <li key = {item.id}>{item.title}</li>)}
         </ul>
       </div> 
   )
