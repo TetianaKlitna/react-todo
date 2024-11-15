0bca96514b1b1104be90d16c6477df6f41f90442
Tetiana Klitna <klitna.tetiana@gmail.com>
2024-11-15 11:31:39 -0800

    Imported useState directly

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
-import React from 'react';
+import { useState } from 'react'
 import TodoList from "./components/TodoList.jsx";
 import AddTodoForm from "./components/AddTodoForm.jsx";
 
 function App() {
 
-  const [newTodo, setNewTodo] = React.useState('');
+  const [newTodo, setNewTodo] = useState('');
 
   return (
     <div>
