8404aac51f1cd702ced4fc1b2ffcd765cc7bf83e
Tetiana Klitna <klitna.tetiana@gmail.com>
2024-12-16 10:15:40 -0800

    swapped button and title

--- src/components/TodoListItem.jsx
+++ src/components/TodoListItem.jsx
@@ -1,10 +1,10 @@
 const TodoListItem = ({ item, onRemoveTodo }) => {
   return (
     <li>
+      {item.title}
       <button onClick={() => onRemoveTodo(item.id)}>
         <strong>X</strong>
       </button>
-      {item.title}
     </li>
   );
 };
